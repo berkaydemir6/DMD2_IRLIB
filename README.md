7f28f83949e3dd9eb8aef829b1d480dd392469ca
Berkay Demir <b3rkaydem1r@yandex.com>
2021-04-15 18:45:31 +0300

    0.0.1

---
+++ README.md
@@ -0,0 +1,15 @@
+# DMD2_IRLIB
+
+Forked from https://github.com/freetronics/DMD2 and fixed IR Remote.
+
+If you installed DMD2 remove it from your library
+Documents -> Arduino -> Library -> DMD2
+
+If you don't have TimerOne. Download and install. https://code.google.com/archive/p/arduino-timerone/downloads
+
+If you don't have IRremote. Download and install. 
+https://github.com/Arduino-IRremote/Arduino-IRremote
+
+You should start with **HelloWorld example.**
+
+## Don't forget star ⭐
---
+++ examples/HelloWorld/HelloWorld.ino
@@ -0,0 +1,51 @@
+#include <DMD2.h>
+#include <IRremote.h>
+#include <SPI.h>
+#include <TimerOne.h>
+#include <fonts/Arial_Black_16.h>
+
+int RECV_PIN = 3;
+int brightness = 165;
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+
+#define DISPLAYS_WIDE 2
+#define DISPLAYS_HIGH 1
+
+SoftDMD dmd(DISPLAYS_WIDE, DISPLAYS_HIGH);
+DMD_TextBox box(dmd, 0, 0, 64, 16); // 32,16 64,16 can you change
+
+#define BUTTON1 16753245
+
+void ArduinoTimerSettings()
+{
+  irrecv.enableIRIn();
+  dmd.scanDisplay();
+}
+
+void setup()
+{
+  Serial.begin(9600);
+  irrecv.enableIRIn();
+  Timer1.initialize(5000);
+  Timer1.attachInterrupt(ArduinoTimerSettings);
+  dmd.clearScreen();
+  dmd.setBrightness(brightness);
+  dmd.selectFont(Arial_Black_16);
+  dmd.begin();
+  dmd.drawString(1, 1, F("Hello"));
+}
+
+void loop()
+{
+  if (irrecv.decode(&results))
+  {
+    Serial.println(results.value);
+    if (results.value == BUTTON1)
+    {
+      box.clear();
+      dmd.drawString(1, 1, F("WORLD"));
+    }
+    irrecv.resume();
+  }
+}
